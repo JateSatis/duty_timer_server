8896d72568644989608aaeb472c6bde9eeaf2891
Maxim <JateSatis@gmail.com>
2024-09-28 10:22:33 +0300

    Made it so that prisma client is also dependent on the NODE_ENV

--- dist/model/config/prismaClient.js
+++ dist/model/config/prismaClient.js
@@ -2,5 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 const client_1 = require("@prisma/client");
-exports.prisma = new client_1.PrismaClient();
+const nodeEnv = process.env.NODE_ENV || "development";
+let datasourceUrl;
+if (nodeEnv === "production") {
+    datasourceUrl = process.env.DATABASE_URL_PROD;
+}
+else {
+    datasourceUrl = process.env.DATABASE_URL_DEV;
+}
+exports.prisma = new client_1.PrismaClient({
+    datasourceUrl,
+});
 //# sourceMappingURL=prismaClient.js.map
--- src/model/config/prismaClient.ts
+++ src/model/config/prismaClient.ts
@@ -1,2 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+
+const nodeEnv = process.env.NODE_ENV || "development";
+
+let datasourceUrl;
+
+if (nodeEnv === "production") {
+  datasourceUrl = process.env.DATABASE_URL_PROD;
+} else {
+  datasourceUrl = process.env.DATABASE_URL_DEV;
+}
+
+export const prisma = new PrismaClient({
+  datasourceUrl,
+});
