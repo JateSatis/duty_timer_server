b75561979b8bb6989669446747888eb955050238
Maxim Danilov <maxdanilovdev@gmail.com>
2024-10-28 21:40:18 +0300

    Change vite config file to explicitly state input file

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,14 @@ export default defineConfig({
     outDir: "dist", // Output directory
     minify: "esbuild", // Use esbuild for fast and optimized minification
     sourcemap: false, // Disable sourcemaps in production
-    target: "esnext", // Optimize for modern browsers/Node.js environments
+		target: "esnext", // Optimize for modern browsers/Node.js environments
+		manifest: true,
+		rollupOptions: {
+      input: "./src/index.ts", // Specify your server entry point
+      output: {
+        entryFileNames: "[name].js", // Output main entry as [name].js
+        format: "cjs", // Use CommonJS format for Node.js compatibility
+      },
+    },
   },
 });
